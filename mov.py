b292dbddab3c32fe898043e6ed41e0da123e5263
Andxre <apd6710@gmail.com>
2019-05-19 16:47:07 -0700

    First commit using the command line

---
+++ mov.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup 
+
+def search(title): 
+	site = "https://www.123movies.gdn/?s=" + title
+	r = requests.get(site)
+	data = r.content
+	soup = BeautifulSoup(data, "html.parser")
+	links = soup.find_all("a", { "class": "ml-mask jt"})
+	for link in links:
+		print(link.text)
+		print(link['href'])
+	
+
+def main():
+	print("Movie Site Searcher")
+	print("Created By: Andre Dasalla")
+	print("Version 1.0")
+
+	title = input("\nEnter Title of Movie: ")
+	title = title.replace(" ", "+")
+	print('\nSearching for links containing "{}"'.format(title))
+	search(title)
+	
+
+main()
