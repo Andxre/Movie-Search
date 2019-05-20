3c6eaf2b5558e397ef947fa4b6edf6871e6e6f6e
Andxre <apd6710@gmail.com>
2019-05-19 17:49:35 -0700

    exact matching

--- mov.py
+++ mov.py
@@ -1,22 +1,27 @@
 
 '''
 Andre Dasalla
-Version 1.0.0
+Version 1.0.1
 May 20, 2019
 '''
 
 import requests
 from bs4 import BeautifulSoup 
 
-def search(title): 
-	site = "https://www.123movies.gdn/?s=" + title
+def search(newTitle, title): 
+	site = "https://www.123movies.gdn/?s=" + newTitle
 	r = requests.get(site)
 	data = r.content
 	soup = BeautifulSoup(data, "html.parser")
 	links = soup.find_all("a", { "class": "ml-mask jt"})
 	for link in links:
-		print(link.text)
-		print(link['href'])
+		if (link.text.lower() == title.lower()):
+			print(link.text)
+			print(link['href'])
+			break
+		else:
+			print(link.text)
+			print(link['href'])
 	
 
 def main():
@@ -25,9 +30,9 @@ def main():
 	print("Version 1.0")
 
 	title = input("\nEnter Title of Movie: ")
-	title = title.replace(" ", "+")
+	newTitle = title.replace(" ", "+")
 	print('\nSearching for links containing "{}"'.format(title))
-	search(title)
+	search(newTitle, title)
 	
 
 main()
